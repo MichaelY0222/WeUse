5c70dc9ff30bad4a28dccaff9313c325212698f9
Lucas-Hsu <lucas1010@163.com>
2024-03-30 11:26:10 +0800

    Add another item to itemList.js
    Added new calculator item to itemList.js.
    
    I will use this to test my changes to the index view boxes.

--- WeUse/itemList.js
+++ WeUse/itemList.js
@@ -2,6 +2,7 @@
 const itemList = [
   { id: 1, name: 'CASIO fx-991CN X', quantity: 3, grades: '8-12', subject: 'Math', contributor: 'system', imgUrl: '../../images/CASIO fx-991CN X.png', stamps: 80, needsApproval: false},
   { id: 2, name: 'CASIO fx-991CN X', quantity: 1, grades: '8-12', subject: 'Math', contributor: 'system', imgUrl: '../../images/CASIO fx-991CN X.png', stamps: 80, needsApproval: false},
+  { id: 3, name: 'CASIO fx-991CN A', quantity: 2, grades: '1-7', subject: 'Maths', contributor: 'system', imgUrl: '../../images/CASIO fx-991CN X.png', stamps: 10, needsApproval: false},
 //Will be transfered to cloud server
 ];
 // Export the userCredentials array
