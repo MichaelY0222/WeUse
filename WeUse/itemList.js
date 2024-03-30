445f9327d477803353b073ff4d08318276241609
Lucas-Hsu <lucas1010@163.com>
2024-03-30 19:35:16 +0800

    Add item Advanced Mathematics
    Added Algebra II textbook to test item name searching.

--- WeUse/itemList.js
+++ WeUse/itemList.js
@@ -3,6 +3,8 @@ const itemList = [
   { id: 1, name: 'CASIO fx-991CN X', quantity: 3, grades: '8-12', subject: 'Math', contributor: 'system', imgUrl: '../../images/CASIO fx-991CN X.png', stamps: 80, needsApproval: false},
   { id: 2, name: 'CASIO fx-991CN X', quantity: 1, grades: '8-12', subject: 'Math', contributor: 'system', imgUrl: '../../images/CASIO fx-991CN X.png', stamps: 80, needsApproval: false},
   { id: 3, name: 'CASIO fx-991CN A', quantity: 2, grades: '1-7', subject: 'Maths', contributor: 'system', imgUrl: '../../images/CASIO fx-991CN X.png', stamps: 10, needsApproval: false},
+  { id: 4, name: 'Advanced Mathematics', quantity: 9, grades: '10', subject: 'Algebra II', contributor: 'system', imgUrl: '../../images/Advanced Mathematics.png', stamps: 200, needsApproval: false},
+  { id: 5, name: 'Advanst Mathematik', quantity: 9, grades: '11', subject: 'Algebra III', contributor: 'system', imgUrl: '../../images/Advanced Mathematics.png', stamps: 200, needsApproval: false},
 //Will be transfered to cloud server
 ];
 // Export the userCredentials array
