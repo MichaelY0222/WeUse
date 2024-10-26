5672252461cd33f5bca57fd383d7728835442e61
Lucas-Hsu <lucas1010@163.com>
2024-10-26 23:39:17 +0800

    empty student

--- WeUse/studentData.js
+++ WeUse/studentData.js
@@ -3,6 +3,7 @@ const studentData = [
   {studentId: 'G2016010165', name: '郁麦可', grade: 10, classNum: 3},
   {studentId: 'G2019010118', name: '徐翊文', grade: 10, classNum: 6},
   {studentId: 'G2017010435', name: '薛嘉毅', grade: 10, classNum: 7},
+  {studentId: '', name: '', grade: 10, classNum: 7},
 //Will be transfered to cloud server
 ];
 // Export the userCredentials array
