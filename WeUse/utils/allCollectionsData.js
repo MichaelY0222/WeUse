baeaa997a273289b68a865c1dd88e3194a0065e6
Michael Yu <115764282+MichaelY0222@users.noreply.github.com>
2025-03-03 20:23:55 +0800

    Create allCollectionsData.js
    Created allCollectionsData to quickly fetch all data from a single database as backup.

---
+++ WeUse/utils/allCollectionsData.js
@@ -0,0 +1,31 @@
+export default async function allCollectionsData(db, collectionName) {
+  const MAX_LIMIT = 20.0;
+  let dbCollection;
+  if (whereClause === undefined) {
+    dbCollection = db.collection(collectionName);
+  } else {
+    dbCollection = db.collection(collectionName).where({
+      ...whereClause
+    });
+  }
+  const countResult = await dbCollection.count();
+  const total = countResult.total;
+  const batchTimes = Math.ceil(total/MAX_LIMIT);
+  const tasks = [];
+  for (let i=0;i<batchTimes;i++) {
+    const promise = dbCollection.skip(i*MAX_LIMIT).limit(MAX_LIMIT).get();
+    tasks.push(promise);
+  }
+  if (batchTimes===0) {
+    return {
+      data: [],
+      errMsg: "empty",
+    }
+  }
+  return (await Promise.all(tasks)).reduce((acc, cur) => {
+    return {
+      data: acc.data.concat(cur.data),
+      errMsg: acc.errMsg,
+    }
+  })
+}
